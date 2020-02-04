7a2f8b5280afd642c037d981d68a7d6f938caca0
Alexander Almström <alexander.almstrom@panagora.se>
2020-02-04 14:39:25 +0100

    Markdown shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const md = require('markdown-it')();
+
 module.exports = function (config) {
+  config.addShortcode('md', content => md.render(content));
+
   return {
     dir: {
       input: 'src',
