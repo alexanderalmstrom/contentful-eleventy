d935f6b9b3af42989ddc6dd1049cf4920b9eaea7
Alexander Almström <alexander.almstrom@panagora.se>
2020-02-06 13:41:05 +0100

    Contentful preview env variable

--- src/services/contentful.js
+++ src/services/contentful.js
@@ -4,7 +4,8 @@ const { createClient } = require('contentful');
 
 const contentful = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  accessToken: process.env.CONTENTFUL_PREVIEW_TOKEN || process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: process.env.CONTENTFUL_PREVIEW_TOKEN ? 'preview.contentful.com' : 'cdn.contentful.com'
 });
 
 module.exports = contentful;
