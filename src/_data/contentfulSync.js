a7401944097236664e0c34d555a17b876dc08965
Alexander Almström <alexander.almstrom@panagora.se>
2020-02-13 17:34:53 +0100

    Testing sync

--- src/_data/contentfulSync.js
+++ src/_data/contentfulSync.js
@@ -2,43 +2,64 @@ const fs = require('fs-extra');
 const contentful = require('../services/contentful');
 const contentfulSyncPath = './contentfulSync.json';
 
-const contentfulSync = async ({ nextSyncToken, ...rest }) => {
+const contentfulSync = async ({ nextSyncToken, ...options }) => {
   return contentful.sync(
     nextSyncToken ?
     {
       nextSyncToken
     } : {
       initial: true,
-      ...rest
+      ...options
     }
   ).then(response => JSON.parse(response.stringifySafe()));
 }
 
-const writeToContentfulJson = async ({ nextSyncToken, ...rest }) => {
-  const json = await fs.readJson(contentfulSyncPath);
-
-  if (nextSyncToken !== json.nextSyncToken) {
-    contentfulSync({ nextSyncToken });
-
-    fs.outputJson(contentfulSyncPath, {
-      nextSyncToken,
-      ...rest
-    });
-  }
+const writeContentfulJson = async ({ nextSyncToken, ...rest }) => {
+  fs.outputJsonSync(contentfulSyncPath, {
+    nextSyncToken,
+    ...rest
+  });
 }
 
-const mapEntryByLocale = ({ fields }, locale = 'en-US') => (
+const mapEntryByLocale = ({ fields }, locale = process.env.CONTENTFUL_DEFAULT_LOCALE || 'en-US') => (
   Object.keys(fields).reduce((state, key) => ({
     ...state,
     [key]: (fields[key][locale] || null)
   }), {})
 )
 
-module.exports = async () => {
-  const sync = await contentfulSync({ type: 'all' });
+const runSync = ({ ...options }) => {
+  return options.initial ? initialSync(options) : nextSync(options);
+}
+
+const initialSync = async (options) => {
+  const sync = await contentfulSync({ ...options });
+  const json = fs.readJsonSync(contentfulSyncPath, { throws: false });
   const entries = sync.entries.map(entry => mapEntryByLocale(entry));
 
-  setInterval(()=> writeToContentfulJson(sync.nextSyncToken, entries), 10000);
+  if (!json) {
+    writeContentfulJson({ nextSyncToken: sync.nextSyncToken, ...entries });
+  }
+
+  return entries;
+}
+
+const nextSync = async () => {
+  const json = await fs.readJsonSync(contentfulSyncPath, { throws: false });
+  const sync = contentfulSync({ nextSyncToken: json.nextSyncToken });
+  const entries = sync.entries.map(entry => mapEntryByLocale(entry));
+
+  if (sync.nextSyncToken !== json.nextSyncToken) {
+    writeContentfulJson({ nextSyncToken: sync.nextSyncToken, ...entries });
+  }
+
+  return entries;
+}
+
+module.exports = async () => {
+  const entries = runSync({ initial: true });
+
+  setInterval(runSync, 10000);
 
   return { entries };
 };
